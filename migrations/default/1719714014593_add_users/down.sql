4d14869fa4a055f213aeb6afff15ef4025f1a029
ohjeo <ojm1818@gmail.com>
2024-06-29 19:22:11 -0700

    Initial commit

---
+++ migrations/default/1719714014593_add_users/down.sql
@@ -0,0 +1,10 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   email TEXT NOT NULL UNIQUE
+-- );
+--
+-- INSERT INTO users (name, email) VALUES ('John', 'john.doe@hasura.io');
+-- INSERT INTO users (name, email) VALUES ('Jane', 'jane.doe@hasura.io');
---
+++ migrations/default/1719714014593_add_users/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO users (name, email) VALUES ('John', 'john.doe@hasura.io');
+INSERT INTO users (name, email) VALUES ('Jane', 'jane.doe@hasura.io');
